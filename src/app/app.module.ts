cea00f97c2f849fd606c0de567ed6c5bcb6d07f4
nashipae <nwariara@safaricomdigitalacademy.co.ke>
2019-05-07 12:47:33 +0300

    add service,error handling &dependency injections.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -8,7 +9,18 @@ import { StrikethroughDirective } from './strikethrough.directive';
 import { DateCountPipe } from './date-count.pipe';
 import { GoalFormComponent } from './goal-form/goal-form.component';
 import {FormsModule} from '@angular/forms';
+import { AlertsService } from './alert-service/alerts.service';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpModule } from '@ngx-progressbar/http';
+import { AboutComponent } from './about/about.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
 
+const routes: Routes = [
+  { path: "goals", component: GoalComponent },
+  { path: "about", component: AboutComponent },
+  { path: '**', component: NotFoundComponent }
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,13 +28,22 @@ import {FormsModule} from '@angular/forms';
     GoalDetailsComponent,
     StrikethroughDirective,
     DateCountPipe,
-    GoalFormComponent
+    GoalFormComponent,
+    AboutComponent,
+    NotFoundComponent,
+
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    NgProgressModule,// normal progress bar
+    NgProgressHttpModule, // progress bar to load http requests
+    RouterModule
   ],
-  providers: [],
+  providers: [AlertsService],// Add service to providers
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
--- src/app/goal.ts
+++ src/app/goal.ts
@@ -3,6 +3,10 @@ export class Goal {
   public showDescription:boolean;
 
   constructor(public id:number, public name: string, public description:string, public completeDate: Date){
+
+  //  hide details
     this.showDescription=false;
+
+
   }
 }
